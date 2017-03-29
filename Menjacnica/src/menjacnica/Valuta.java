d50bae12fe3600f8cb683140ba08b6baf86abcec
UrosMomcilovic <urosuki968@gmail.com>
2017-03-29 09:59:21 +0200

    Generisane metode toString, equals i hashCode, toString prilagodjena klasama

--- Menjacnica/src/menjacnica/Valuta.java
+++ Menjacnica/src/menjacnica/Valuta.java
@@ -27,8 +27,50 @@ public class Valuta {
 		this.kursNaDan = kursNaDan;
 	}
 	
-	
-	
+	@Override
+	public String toString() {
+		String s= "Valuta naziv=" + naziv + ", skraceniNaziv=" + skraceniNaziv + ", kursevi: ";
+		
+		for (int i = 0; i < kursNaDan.size(); i++) {
+			s+=kursNaDan.get(i);
+		}
+		return s;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((kursNaDan == null) ? 0 : kursNaDan.hashCode());
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		result = prime * result + ((skraceniNaziv == null) ? 0 : skraceniNaziv.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Valuta other = (Valuta) obj;
+		if (kursNaDan == null) {
+			if (other.kursNaDan != null)
+				return false;
+		} else if (!kursNaDan.equals(other.kursNaDan))
+			return false;
+		if (naziv == null) {
+			if (other.naziv != null)
+				return false;
+		} else if (!naziv.equals(other.naziv))
+			return false;
+		if (skraceniNaziv == null) {
+			if (other.skraceniNaziv != null)
+				return false;
+		} else if (!skraceniNaziv.equals(other.skraceniNaziv))
+			return false;
+		return true;
+	}
 	
 	
 }
