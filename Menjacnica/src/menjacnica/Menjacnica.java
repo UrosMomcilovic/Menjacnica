0b27c9e7eb3c7b23c373fda83b62144d68b1d30a
UrosMomcilovic <urosuki968@gmail.com>
2017-03-29 21:27:22 +0200

    Implementirane metode u okviru master grane

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,26 +1,32 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import menjacnica.interfejs.menjacnicainterfejs;
 
 public class Menjacnica implements menjacnicainterfejs {
+	
+	LinkedList<Valuta> valute = new LinkedList<Valuta>();
 
 	@Override
 	public void dodajKursNaDan(String naziv, GregorianCalendar dan, double srednji, double kupovni, double prodajni) {
-		// TODO Auto-generated method stub
+		
+		valute.getFirst().getKursNaDan().add(new Kurs());
 
 	}
 
 	@Override
 	public void obrisiKursNaDan(String naziv, GregorianCalendar dan) {
-		// TODO Auto-generated method stub
-
+		
+		valute.getFirst().getKursNaDan().removeFirst();
+		
 	}
 
 	@Override
 	public void pronadjiKursNaDan(String naziv, GregorianCalendar dan) {
-		// TODO Auto-generated method stub
+		
+		System.out.println(valute.getFirst().getKursNaDan());
 
 	}
 
