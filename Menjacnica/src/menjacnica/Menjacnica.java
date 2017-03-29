ed62a9286897476c29b69839cf64bf731846af8a
UrosMomcilovic <urosuki968@gmail.com>
2017-03-29 21:02:25 +0200

    Kreirana klasa Menjacnica koja implementira interfejs menjacnica interfejs, metode ostavljene sa praznim telom

---
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -0,0 +1,27 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+import menjacnica.interfejs.menjacnicainterfejs;
+
+public class Menjacnica implements menjacnicainterfejs {
+
+	@Override
+	public void dodajKursNaDan(String naziv, GregorianCalendar dan, double srednji, double kupovni, double prodajni) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void obrisiKursNaDan(String naziv, GregorianCalendar dan) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pronadjiKursNaDan(String naziv, GregorianCalendar dan) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
